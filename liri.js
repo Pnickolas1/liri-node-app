4918f41f0f08c512a6344afa0bcfc9f37b54c330
Peter Tountas <petertountas1@gmail.com>
2016-12-11 16:49:51 -0600

    logic for twitter read

--- liri.js
+++ liri.js
@@ -2,6 +2,10 @@ var Twitter = require('twitter');
 
 var keysTokens = require("./keys.js")
 
+// takes the argument and tells the application logic to follow
+var command = process.argv[2];
+
+// twitter api keys & tokens
 var client = new Twitter({
   consumer_key: keysTokens.twitterKeys.consumer_key,
   consumer_secret: keysTokens.twitterKeys.consumer_secret,
@@ -9,10 +13,23 @@ var client = new Twitter({
   access_token_secret: keysTokens.twitterKeys.access_token_secret
 });
  
-var params = {screen_name: 'Pnickolas'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+
+// THESE ARE PARAMETERS BEING PASSED INTO THE TWITTER SEARCH
+var params = {screen_name: 'Pnickolas',
+			count: 1};
+
+
+
+
+if(command === 'twitter'){
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	  	console.log(tweets);
+	  }
+	});
+
+	} else if(command != 'twitter'){
+		console.log('please log \'twitter\'');
+};
+
 

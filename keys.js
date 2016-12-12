d8ea132afaeba1fcb15143b6933b846b39867a7c
Peter Tountas <petertountas1@gmail.com>
2016-12-12 10:23:51 -0600

    remove keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('Twitter API keys loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'b3gTye1i85uD0ebk5s3Ljfmq1',
-  consumer_secret: '9PjYGsBYHyRLfoq9irDIhzrJzsK60BtOjQn1cDE8nr1QpEnVcS',
-  access_token_key: '176422957-I94MvWUYVMKiPRfdoAtWI2tRzMzx9kht8b9LUAhJ',
-  access_token_secret: 'ax9aUZb9f9izZN6vBOzeP3uK2NmVbdDUduaD394B3Zcwn',
-}

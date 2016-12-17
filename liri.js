79ac3e1fe2a45b0c2dda505967b92448b62e5259
Peter Tountas <petertountas1@gmail.com>
2016-12-16 20:42:23 -0600

    more changes

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs');
 
 // NPM MODULE FOR GETTING API DATA FROM OMDB
@@ -19,50 +18,80 @@ var songTitle = process.argv[3];
 
 // twitter api keys & tokens
 var client = new Twitter({
-  consumer_key: keysTokens.twitterKeys.consumer_key,
-  consumer_secret: keysTokens.twitterKeys.consumer_secret,
-  access_token_key: keysTokens.twitterKeys.access_token_key,
-  access_token_secret: keysTokens.twitterKeys.access_token_secret
-}); 
+    consumer_key: keysTokens.twitterKeys.consumer_key,
+    consumer_secret: keysTokens.twitterKeys.consumer_secret,
+    access_token_key: keysTokens.twitterKeys.access_token_key,
+    access_token_secret: keysTokens.twitterKeys.access_token_secret
+});
 
 // THESE ARE PARAMETERS BEING PASSED INTO THE TWITTER SEARCH
-var params = {screen_name: 'Pnickolas',
-			count: 15,
-			};
+var params = {
+    screen_name: 'Pnickolas',
+    count: 15,
+};
 
-if(command === 'my-tweets'){
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-	  if (!error) {
-	  		console.log("----- Pnickolas tweets -----")
-	  	for (var i = 0; i < tweets.length; i++) {
-	  		console.log(tweets[i].text);
-	  		console.log('\n');
-	  	}
-	  		console.log("----- END -----");
-	  }
-	});
-} else if(command === 'movie-this'){
+if (command === 'my-tweets') {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log("----- Pnickolas tweets -----")
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+                console.log('\n');
+            }
+            console.log("----- END -----");
+        }
+    });
+} else if (command === 'movie-this') {
 
-	inquirer.prompt([{
-				name: 'name',
-				message: "Movie name:"
-	 		}]).then(function(answers){
+    inquirer.prompt([{
+        name: 'name',
+        message: "Movie name:"
+    }]).then(function(answers) {
 
-	 			request("http://www.omdbapi.com/?t=" +  answers.name +"&y=&plot=full&r=json&tomatoes=true", function(error, response, body) {
-	 			
-	 			if (!error && response.statusCode === 200) {
+        request("http://www.omdbapi.com/?t=" + answers.name + "&y=&plot=full&r=json&tomatoes=true", function(error, response, body) {
 
-	 				console.log("\n" + '	Movie: ' + JSON.parse(body).Title );
-	 				console.log("\n")
-	 				console.log("Released: " + JSON.parse(body).Released);
-	 				console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-	 				console.log("Produced in: " + JSON.parse(body).Country);
-	 				console.log("Language: " + JSON.parse(body).Language);
-	 				console.log("Plot: " + JSON.parse(body).Plot);
-	 				console.log("Actors: " + JSON.parse(body).Actors);
-	 				console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-	 				console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL + "\n");
-	 			}
-	 	});
-	});
+            if (!error && response.statusCode === 200) {
+
+                console.log("\n" + '	Movie: ' + JSON.parse(body).Title);
+                console.log("\n")
+                console.log("Released: " + JSON.parse(body).Released);
+                console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+                console.log("Produced in: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+                console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL + "\n");
+            }
+        });
+    });
+} else if (command === 'spotify-this-song') {
+
+    inquirer.prompt([{
+        name: 'name',
+        message: "Search for song title: "
+    }]).then(function(answers) {
+
+        spotify.search({
+            type: 'track',
+            query: answers.name
+        }, function(err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            } else {
+                var items = data.tracks.items
+                for(var i = 0; i < items.length; i++){
+                    
+                   var item = items[i]
+                var song = item.name
+                    console.log(song);
+                var album = item.album;
+                    console.log(album);
+                    }
+                }
+        });
+
+    })
+
 };
